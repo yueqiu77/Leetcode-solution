9f1f3722c73de04bfc5ab289d8f5203f3baeb4c4
sxx7470 <30657871+sxx7470@users.noreply.github.com>
2026-02-13 16:53:36 +0900

    35. Search Insert Position

---
+++ 0035_search_insert_position.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int searchInsert(std::vector<int> &nums, int &target)
+	{
+		int length = nums.size();
+        for (int i = 0; i < length; ++i) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return length;
+	}
+};
